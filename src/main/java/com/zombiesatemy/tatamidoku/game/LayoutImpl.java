62df76e8a37d6ea6fbc4688db1b79ee4da44bb19
Dave Ingram <dave@dmi.me.uk>
2019-09-03 14:34:35 +0100

    Add Group implementation

--- src/main/java/com/zombiesatemy/tatamidoku/game/LayoutImpl.java
+++ src/main/java/com/zombiesatemy/tatamidoku/game/LayoutImpl.java
@@ -2,6 +2,8 @@ package com.zombiesatemy.tatamidoku.game;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LayoutImpl implements Layout {
     private final int mGroupSize;
@@ -34,5 +36,54 @@ public class LayoutImpl implements Layout {
     @Override
     public List<Group> getGroups() {
         return mGroups;
+    }
+
+    static class GroupImpl implements Layout.Group {
+        private final int mFirstColumn;
+        private final int mFirstRow;
+        private final boolean mIsVertical;
+        private final int mSize;
+
+        public GroupImpl(int firstColumn, int firstRow, boolean isVertical, int size) {
+            mFirstColumn = firstColumn;
+            mFirstRow = firstRow;
+            mIsVertical = isVertical;
+            mSize = size;
+        }
+
+        @Override
+        public int getFirstColumn() {
+            return mFirstColumn;
+        }
+
+        @Override
+        public int getFirstRow() {
+            return mFirstRow;
+        }
+
+        @Override
+        public boolean isVertical() {
+            return mIsVertical;
+        }
+
+        @Override
+        public int getSize() {
+            return mSize;
+        }
+
+        @Override
+        public List<CellCoordinate> getCellCoordinates() {
+            final List<CellCoordinate> coords;
+            if (mIsVertical) {
+                coords = IntStream.range(0, mSize)
+                        .mapToObj(row -> new CellCoordinate(mFirstColumn, mFirstRow + row))
+                        .collect(Collectors.toList());
+            } else {
+                coords = IntStream.range(0, mSize)
+                        .mapToObj(col -> new CellCoordinate(mFirstColumn + col, mFirstRow))
+                        .collect(Collectors.toList());
+            }
+            return Collections.unmodifiableList(coords);
+        }
     }
 }
