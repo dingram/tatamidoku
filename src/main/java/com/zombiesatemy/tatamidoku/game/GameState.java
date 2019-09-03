25c3c73f41dedd33ca5cdf813af40d295f6dc7b6
Dave Ingram <dave@dmi.me.uk>
2019-09-03 10:27:33 +0100

    Validate column and row contents

--- src/main/java/com/zombiesatemy/tatamidoku/game/GameState.java
+++ src/main/java/com/zombiesatemy/tatamidoku/game/GameState.java
@@ -8,21 +8,51 @@ public class GameState {
         return mLayout;
     }
 
-    public GameState setLayout(Layout layout) {
+    public void setLayout(Layout layout) {
         mLayout = layout;
-        return this;
     }
 
     public Placement getPlacement() {
         return mPlacement;
     }
 
-    public GameState setPlacement(Placement placement) {
+    public void setPlacement(Placement placement) {
         mPlacement = placement;
-        return this;
     }
 
     public boolean isValid() {
-        return false;
+        final int[][] values = getPlacement().getAllValuesCopy();
+        final int groupSize = getLayout().getGroupSize();
+        final int maxPerColumn = getLayout().getGroupCount();
+        return validateColumns(values, groupSize, maxPerColumn) && validateRows(values, groupSize, maxPerColumn) && validateGroups(values);
+    }
+
+    private boolean validateColumns(int[][] values, int groupSize, int maxPerColumn) {
+        for (int rowNum = 0; rowNum < values.length; rowNum++) {
+            final int[] counts = new int[groupSize];
+            for (final int[] column : values) {
+                final int value = column[rowNum];
+                if (value > 0 && ++counts[value - 1] > maxPerColumn) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean validateRows(int[][] values, int groupSize, int maxPerColumn) {
+        for (final int[] column : values) {
+            final int[] counts = new int[groupSize];
+            for (final int value : column) {
+                if (value > 0 && ++counts[value - 1] > maxPerColumn) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean validateGroups(int[][] values) {
+        return true;
     }
 }
